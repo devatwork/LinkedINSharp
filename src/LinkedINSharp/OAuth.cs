9070af2dde5ec056807e438a652da7ea390b7e6c
Bert Willems <b.willems@premotion.nl>
2012-04-20 12:44:52 +0200

    + added the ExchangeCodeForAccessToken method

--- src/LinkedINSharp/OAuth.cs
+++ src/LinkedINSharp/OAuth.cs
@@ -22,11 +22,11 @@ namespace LinkedINSharp
 		/// Requests authorization of your application by first sending the RequestToken request and then generate an LinkedIN URL to which users can be redirected. This is the first stage of OAuth.
 		/// </summary>
 		/// <param name="callbackUri">The <see cref="Uri"/> to which the user is redirected when returning from the LinkedIN authorization screen.</param>
-		/// <param name="requestTokenSecret">The secret of the request token. Keep this somewhere safely stored with the user's session. You will need the provide this later in method TODO.</param>
+		/// <param name="requestSecret">The secret of the request token. Keep this somewhere safely stored with the user's session. You will need the provide this later in method <see cref="ExchangeCodeForAccessToken"/>.</param>
 		/// <returns>Returns the LinkedIN authorization <see cref="Uri"/> to which users must be redirected in order give access to your application.</returns>
-		/// <exception cref="ArgumentNullException">Thrown whe <paramref name="callbackUri"/> is null.</exception>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="callbackUri"/> is null.</exception>
 		/// <exception cref="LinkedINHttpResponseException">Thrown when LinkedIN did not respond properly while requesting the request token.</exception>
-		public Uri RequestAuthorizationToken( Uri callbackUri, out string requestTokenSecret )
+		public Uri RequestAuthorizationToken( Uri callbackUri, out string requestSecret )
 		{
 			// validate arguments
 			if ( callbackUri == null )
@@ -40,19 +40,63 @@ namespace LinkedINSharp
 			             	};
 
 			// create the request
-			var request = new RestRequest( "requestToken" );
+			var requestTokenRequest = new RestRequest( "requestToken" );
 
 			// execute the request
-			var response = ExecuteRequest( client, request );
+			var requestTokenResponse = ExecuteRequest( client, requestTokenRequest );
 
 			// extract the token from the query string
-			var queryString = HttpUtility.ParseQueryString( response.Content );
-			var oauthToken = queryString[ "oauth_token" ];
-			requestTokenSecret = queryString[ "oauth_token_secret" ];
+			var requestTokenResponseParameters = HttpUtility.ParseQueryString( requestTokenResponse.Content );
+			var oauthToken = requestTokenResponseParameters[ "oauth_token" ];
+			requestSecret = requestTokenResponseParameters[ "oauth_token_secret" ];
 
 			// build the redirect uri
-			request = new RestRequest( "authenticate?oauth_token=" + oauthToken );
-			return client.BuildUri( request );
+			requestTokenRequest = new RestRequest( "authenticate?oauth_token=" + oauthToken );
+			return client.BuildUri( requestTokenRequest );
+		}
+		/// <summary>
+		/// Exchanges the request code for an access token.This is the second stage of OAuth.
+		/// </summary>
+		/// <remarks>
+		/// Make sure you call <see cref="RequestAuthorizationToken"/> first.
+		/// </remarks>
+		/// <param name="requestUri">The <see cref="Uri"/> of the request </param>
+		/// <param name="requestSecret">The request secret, gained when the call to <see cref="RequestAuthorizationToken"/> was made.</param>
+		/// <param name="accessToken">The access token.</param>
+		/// <param name="accessSecret">The access secret.</param>
+		/// <returns>Returns the original <see cref="Uri"/> from which the user came before starting the authorization flow.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="requestUri"/> or <paramref name="requestSecret"/> is null.</exception>
+		/// <exception cref="LinkedINHttpResponseException">Thrown when LinkedIN did not respond properly while requesting the access token.</exception>
+		public void ExchangeCodeForAccessToken( Uri requestUri, string requestSecret, out string accessToken, out string accessSecret )
+		{
+			// validate arguments
+			if ( requestUri == null )
+				throw new ArgumentNullException( "requestUri" );
+			if ( string.IsNullOrEmpty( requestSecret ) )
+				throw new ArgumentNullException( "requestSecret" );
+
+			// extract the token and verifier from the 
+			var requestTokenQueryParameters = HttpUtility.ParseQueryString( requestUri.Query );
+			var requestToken = requestTokenQueryParameters[ "oauth_token" ];
+			var requestVerifier = requestTokenQueryParameters[ "oauth_verifier" ];
+
+			// create the client
+			var client = new RestClient
+			             	{
+			             		BaseUrl = BaseUrl,
+			             		Authenticator = OAuth1Authenticator.ForAccessToken( consumerKey, consumerSecret, requestToken, requestSecret, requestVerifier )
+			             	};
+
+			// create the request
+			var requestAccessTokenRequest = new RestRequest( "accessToken" );
+
+			// execute the request
+			var requestActionTokenResponse = ExecuteRequest( client, requestAccessTokenRequest );
+
+			// extract the token and secret from the response query string
+			var requestActionTokenResponseParameters = HttpUtility.ParseQueryString( requestActionTokenResponse.Content );
+			accessToken = requestActionTokenResponseParameters[ "oauth_token" ];
+			accessSecret = requestActionTokenResponseParameters[ "oauth_token_secret" ];
 		}
 		#endregion
 	}
