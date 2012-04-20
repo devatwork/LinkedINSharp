2bc1118a04d52e616af48e68777fa04367631a76
Bert Willems <b.willems@premotion.nl>
2012-04-20 11:22:37 +0200

    + set assembly properties

--- src/LinkedINSharp/Properties/AssemblyInfo.cs
+++ src/LinkedINSharp/Properties/AssemblyInfo.cs
@@ -1,39 +1,17 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("LinkedINSharp")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LinkedINSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("ad97710b-31ba-42d7-8c58-44c10daba1e9")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle( "LinkedIN#" )]
+[assembly: AssemblyDescription( "C# SDK for LinkedIN" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Premotion, Liones and contributers" )]
+[assembly: AssemblyProduct( "LinkedIN#" )]
+[assembly: AssemblyCopyright( "Copyright © Premotion, Liones and contributers 2012" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+[assembly: ComVisible( false )]
+[assembly: Guid( "ad97710b-31ba-42d7-8c58-44c10daba1e9" )]
+[assembly: AssemblyVersion( "0.1.0.0" )]
+[assembly: AssemblyFileVersion( "0.1.0.0" )]
+[assembly: CLSCompliant( true )]
