6e0832979b0ca994878b67ae6425ad4e9bc23cc9
Bert Willems <b.willems@premotion.nl>
2012-04-20 12:22:24 +0200

    + exceptions

---
+++ src/LinkedINSharp/LinkedINException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace LinkedINSharp
+{
+	/// <summary>
+	/// Base class for all the LinkedIN exceptions.
+	/// </summary>
+	[Serializable]
+	public class LinkedINException : Exception
+	{
+		#region Constructors
+		/// <summary>
+		/// Constructs an empty LinkedIN exception.
+		/// </summary>
+		public LinkedINException()
+		{
+		}
+		/// <summary>
+		/// Constructs a LinkedIN exception with the given <paramref name="message"/>.
+		/// </summary>
+		/// <param name="message">The message descriving the cause of this exception.</param>
+		public LinkedINException( string message ) : base( message )
+		{
+		}
+		/// <summary>
+		/// Constructs a LinkedIN exception with the given <paramref name="message"/>.
+		/// </summary>
+		/// <param name="message">The message descriving the cause of this exception.</param>
+		/// <param name="inner">The inner <see cref="Exception"/>.</param>
+		public LinkedINException( string message, Exception inner ) : base( message, inner )
+		{
+		}
+		/// <summary>
+		/// Serialization constructor.
+		/// </summary>
+		/// <param name="info"></param>
+		/// <param name="context"></param>
+		protected LinkedINException( SerializationInfo info, StreamingContext context ) : base( info, context )
+		{
+		}
+		#endregion
+	}
+}
---
+++ src/LinkedINSharp/LinkedINHttpResponseException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace LinkedINSharp
+{
+	/// <summary>
+	/// This is exception is thrown when a request to the LinkedIN response did not result in the expected response.
+	/// </summary>
+	[Serializable]
+	public class LinkedINHttpResponseException : LinkedINException
+	{
+		#region Constuctor
+		/// <summary>
+		/// Constructs an <see cref="LinkedINHttpResponseException"/>.
+		/// </summary>
+		/// <param name="expectedStatusCode">The expected <see cref="HttpStatusCode"/> of the response.</param>
+		/// <param name="actualStatusCode">The actual <see cref="HttpStatusCode"/> of the response.</param>
+		/// <param name="message">The message descriving the cause of this exception.</param>
+		/// <param name="inner">The inner <see cref="Exception"/>.</param>
+		public LinkedINHttpResponseException( HttpStatusCode expectedStatusCode, HttpStatusCode actualStatusCode, string message, Exception inner ) : base( message, inner )
+		{
+			// set values
+			ExpectedStatusCode = expectedStatusCode;
+			ActualStatusCode = actualStatusCode;
+		}
+		/// <summary>
+		/// Serialization constructor.
+		/// </summary>
+		/// <param name="info"></param>
+		/// <param name="context"></param>
+		protected LinkedINHttpResponseException( SerializationInfo info, StreamingContext context ) : base( info, context )
+		{
+		}
+		#endregion
+		#region Private Fields
+		/// <summary>
+		/// The expected <see cref="HttpStatusCode"/> of the response.
+		/// </summary>
+		public HttpStatusCode ExpectedStatusCode { get; private set; }
+		/// <summary>
+		/// The actual <see cref="HttpStatusCode"/> of the response.
+		/// </summary>
+		public HttpStatusCode ActualStatusCode { get; private set; }
+		#endregion
+	}
+}
